9886639d3a665e75c0119e86196f91b94658f3dd
DanPearce <daniel_pearce@outlook.com>
2022-02-20 19:14:02 +0000

    Add audio files to JavaScript

---
+++ assets/js/audio.js
@@ -0,0 +1,8 @@
+//Document Get Area
+const correctAudio = new Audio('assets/audio/correct.mp3');
+const incorrectAudio = new Audio('assets/audio/incorrect/wav');
+const clickAudio = new Audio('assets/audio/click.wav');
+
+//Event Listeners
+
+//Functions
