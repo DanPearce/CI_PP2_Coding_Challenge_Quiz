b4b2d5838058bb090694a683159156b1700ad232
DanPearce <daniel_pearce@outlook.com>
2022-02-20 19:18:39 +0000

    Add audio icons to JavaScript via the DOM and add click event listeners

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -2,7 +2,11 @@
 const correctAudio = new Audio('assets/audio/correct.mp3');
 const incorrectAudio = new Audio('assets/audio/incorrect/wav');
 const clickAudio = new Audio('assets/audio/click.wav');
+const audioOffButton = document.getElementById('audio-off');
+const audioOnButton = document.getElementById('audio-on');
 
 //Event Listeners
+audioOffButton.addEventListener('click', audioOff);
+audioOnButton.addEventListener('click', audioOn);
 
 //Functions
