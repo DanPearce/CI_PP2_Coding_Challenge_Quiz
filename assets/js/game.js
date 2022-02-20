fb5cea9299f00f4eac86b4b4f2548f1b4e180d86
DanPearce <daniel_pearce@outlook.com>
2022-02-20 13:15:13 +0000

    Add functions to game.js

---
+++ assets/js/game.js
@@ -0,0 +1,45 @@
+
+//Functions
+function startGameEasy(){
+
+}
+
+function startGameModerate(){
+
+}
+
+function startGameHard(){
+  
+}
+
+function nextQuestion(){
+
+}
+
+function showQuestion(){
+
+}
+
+function resetQuestion(){
+
+}
+
+function answerClicked(){
+
+}
+
+function setAnswerClickedClass(){
+
+}
+
+function removeAnswerClickedClass(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementIncorrectScore(){
+  
+}
