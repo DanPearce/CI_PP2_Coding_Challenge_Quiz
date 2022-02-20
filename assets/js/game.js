38dc2c5ae06ffedff04bbbf65ef764ef12c03b62
DanPearce <daniel_pearce@outlook.com>
2022-02-20 13:28:12 +0000

    Add event listeners for easy/moderate/hard btns

--- assets/js/game.js
+++ assets/js/game.js
@@ -4,6 +4,11 @@ const moderateButton = document.getElementById('moderate-btn');
 const hardButton = document.getElementById('hard-btn');
 const chooseDifficulty = document.getElementById('difficulty');
 
+//Event Listeners
+easyButton.addEventListener('click', startGameEasy);
+moderateButton.addEventListener('click', startGameModerate);
+hardButton.addEventListener('click', startGameHard);
+
 //Functions
 function startGameEasy(){
 
