4f4ead545d2d0c573708fbcb0147951f526e1a71
DanPearce <daniel_pearce@outlook.com>
2022-02-20 20:20:55 +0000

    Fix file issue in audio.js

--- assets/js/audio.js
+++ assets/js/audio.js
@@ -1,6 +1,6 @@
 //Document Get Area
 const correctAudio = new Audio('assets/audio/correct.mp3');
-const incorrectAudio = new Audio('assets/audio/incorrect/wav');
+const incorrectAudio = new Audio('assets/audio/incorrect.wav');
 const clickAudio = new Audio('assets/audio/click.wav');
 const audioOffButton = document.getElementById('audio-off');
 const audioOnButton = document.getElementById('audio-on');
