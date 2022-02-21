3bc1945a503f8cc93ad40e8f8d53880901d0864f
DanPearce <daniel_pearce@outlook.com>
2022-02-21 11:55:14 +0000

    Add submit event listener and email js code

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,12 +1,17 @@
 //Document Get Functions
-const name = document.getElementById('name');
-const email = document.getElementById('email');
-const message = document.getElementById('message');
-const project = document.getElementById('project');
 const submit = document.getElementById('submit'); 
 
+submit.addEventListener('submit', sendEmail);
+
+//Functions
 function sendEmail(){
-    emailjs.init("user_cv6MUQhR9JkAaQYC30WwK");
+  preventDefault();
+  emailjs.init("user_cv6MUQhR9JkAaQYC30WwK");
+  const name = document.getElementById('name');
+  const email = document.getElementById('email');
+  const message = document.getElementById('message');
+  const project = document.getElementById('project');
+  emailjs.send('service_i7kt59p', 'service_i7kt59p', this)
 }
 
 function emailSent(){
