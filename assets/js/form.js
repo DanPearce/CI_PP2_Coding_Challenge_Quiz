6fb6b4a875b90767e761dcf1ca98173a351278d4
DanPearce <daniel_pearce@outlook.com>
2022-02-21 11:43:20 +0000

    Add document IDs to script via the DOM

--- assets/js/form.js
+++ assets/js/form.js
@@ -1,8 +1,14 @@
+//Document Get Functions
+const name = document.getElementById('name');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const project = document.getElementById('project');
+const submit = document.getElementById('submit'); 
 
 function sendEmail(){
-
+    emailjs.init("user_cv6MUQhR9JkAaQYC30WwK");
 }
 
 function emailSent(){
-  
+
 }
