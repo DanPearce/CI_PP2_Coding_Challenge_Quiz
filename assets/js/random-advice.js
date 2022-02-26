de128128ab79572f22940476f2840206f943c456
DanPearce <daniel_pearce@outlook.com>
2022-02-26 19:49:42 +0000

    Add catch error log to the console and display default message

--- assets/js/random-advice.js
+++ assets/js/random-advice.js
@@ -9,5 +9,8 @@ window.onload = function getRandomAdvice(){
   }).then(adviceJson => {
     const randomAdvice = adviceJson.slip;
     advice.innerText = randomAdvice.advice;
+  }).catch(error => {
+    console.log(error);
+    advice.innerText = "Our random advice machine appears to be broken! Please report this! So instead our advice is simply stay out of trouble!"
   });
 }
